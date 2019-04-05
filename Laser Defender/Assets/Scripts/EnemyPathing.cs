0028698b54441ca5c368f044c387667e11566e4c
SKosinski <stasiu.kosinski@hotmail.com>
2019-04-05 12:58:43 +0200

    before multienemy spawn

---
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] WaveConfig waveConfig;
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    [SerializeField] int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AddPath();
+        waypoints = waveConfig.getWaypoints();
+        transform.position = waypoints[0].transform.position;
+
+    }
+    /*
+    private void AddPath()
+    {
+        for (int i=0; i<path.transform.childCount; i++)
+        {
+            waypoints.Add(path.transform.GetChild(i));
+        }
+    }
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movementThisFrame);
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -8,6 +8,12 @@ public class Player : MonoBehaviour
 
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 0.1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.3f;
+
+
+    Coroutine firingCoroutine;
 
     float xMin;
     float xMax;
@@ -20,6 +26,38 @@ public class Player : MonoBehaviour
         SetUpMoveBoundaries();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            GameObject laser = Instantiate(
+                laserPrefab,
+                transform.position,
+                Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+
     private void SetUpMoveBoundaries()
     {
         Camera gameCamera = Camera.main;
@@ -28,12 +66,6 @@ public class Player : MonoBehaviour
 
         yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0 + padding, 0)).y;
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1 - padding, 0)).y;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
     }
 
     private void Move()
